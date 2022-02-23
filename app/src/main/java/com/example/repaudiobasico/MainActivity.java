fae5b402048734377052c7c0179a4bcb857aa7d7
Fernando Fernández <heisenferg@gmail.com>
2022-02-23 19:05:00 +0100

    Spotify

--- app/src/main/java/com/example/repaudiobasico/MainActivity.java
+++ app/src/main/java/com/example/repaudiobasico/MainActivity.java
@@ -5,31 +5,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MediaController.MediaPlayerControl {
     MediaPlayer mediaPlayer;
-    ImageView caratula;
+    MediaController mc;
+    Handler h;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mediaPlayer = MediaPlayer.create(this, R.raw.duality);
-        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+        mc = new MediaController(this);
+        mc.setMediaPlayer(this);
+        mc.setAnchorView(findViewById(R.id.imageViewCaratula));
+        h = new Handler();
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                h.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mc.show(0);
+                    }
+                });
+            }
+        });
+
+   /*     mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
                 TextView estado = findViewById(R.id.textViewEstado);
                 mediaPlayer.release();
                 estado.setText("Released!");
             }
-        });
+        });*/
     }
 
+    public void Show(View v){
+        mc.show();
+    }
+
+    public void Hide(View v){
+        mc.hide();
+    }
+/*
     public void play(View view) {
         TextView estado = findViewById(R.id.textViewEstado);
         if (mediaPlayer.isPlaying()){
@@ -64,5 +92,75 @@ public class MainActivity extends AppCompatActivity {
         } else {
             estado.setText("La música ya estaba parada.");
         }
+    }*/
+
+    @Override
+    public void start() {
+    if (!mediaPlayer.isPlaying()){
+        mediaPlayer.start();
     }
+    }
+
+    @Override
+    public void pause() {
+        if (mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        }
+    }
+
+    @Override
+    public int getDuration() {
+        return mediaPlayer.getDuration();
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return mediaPlayer.getCurrentPosition();
+    }
+
+    @Override
+    public void seekTo(int pos) {
+        mediaPlayer.seekTo(pos);
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return mediaPlayer.isPlaying();
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return true;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return mediaPlayer.getAudioSessionId();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction()==MotionEvent.ACTION_DOWN)
+            if(!mc.isShowing())
+                mc.show(0);
+            else
+                mc.hide();
+        return false;
+    }
+
 }
